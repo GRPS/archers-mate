7bc3b82e6eb450ebf552976dea8dc2645538892d
OVAL\Graham.Simmons <grahamsimmons@hotmail.co.uk>
2018-08-08 15:34:42 +0100

    Added models, pipe, providers and lazy loaded home page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,62 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 
+import { Const } from '../../providers/constants';
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	loading;
+	haveLoadedRounds: boolean = false;
+	haveLoadedBows: boolean = false;
 
-  }
+	constructor(
+				public navCtrl: NavController, 
+				public loadingCtrl: LoadingController,
+				public navParams: NavParams
+				) {
+
+		this.loading = this.loadingCtrl.create({
+			content: 'Please data ...'
+		});
+		
+	}
+
+	//Do once when page first loads.
+	ionViewDidLoad() {
+		Const.MISC.CURRENT_PAGE = 'HomePage';
+		console.log( Const.MISC.CURRENT_PAGE + ': ionViewWillEnter' );
+	}
+
+	//Do before page becomes active.
+	ionViewWillEnter() {}
+
+	//Do after page becomes active.
+	ionViewDidEnter() {}
+
+	//Do before page leaves.
+	ionViewWillLeave() {}
+
+	//Do after page leaves.
+	ionViewDidLeave() {}
+
+	//Do when page completely removed.
+	ionViewWillUnload() {}
+
+	gotoScore() {
+		// this.navCtrl.push( ScorePage );
+	}
+
+	gotoHistory() {
+		// this.navCtrl.push( HistoryPage );
+	}
+
+	gotoSettings() {
+		// this.navCtrl.push( SettingsPage );
+	}
 
 }
