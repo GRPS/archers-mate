669883e45b5ed3e447020fb222fc30726cef1e6b
OVAL\Graham.Simmons <grahamsimmons@hotmail.co.uk>
2018-08-15 15:19:00 +0100

    Add resources and trap Android back button from within settings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,51 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { App, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Const } from '../providers/constants';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'HomePage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+	rootPage:any = 'HomePage';
+
+	constructor(
+				private app: App, 
+				private platform: Platform, 
+				private statusBar: StatusBar, 
+				private splashScreen: SplashScreen
+			) {
+
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			statusBar.styleDefault();
+			splashScreen.hide();
+
+			// Handle Android back button
+			platform.registerBackButtonAction( () => {
+
+				let navCtrl = this.app.getActiveNavs()[0];
+				let activePage = navCtrl.getActive();
+
+				switch ( activePage.name ) {
+					case Const.PAGES.SETTINGS.GENERAL :
+					case Const.PAGES.SETTINGS.BOWS :
+					case Const.PAGES.SETTINGS.SHOOTERS :
+					case Const.PAGES.SETTINGS.ROUNDS :
+						navCtrl.setRoot( Const.PAGES.HOME );
+						break;
+					default:
+						navCtrl.pop();
+						break;
+				}
+
+			});
+
+    	});
+	}
+
 }
 
